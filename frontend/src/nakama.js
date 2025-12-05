2472eabc1e0e0cf53cb3a058b11ba017735da6fc
IJPenguin <ishanjaiswal370@gmail.com>
2025-12-05 11:57:41 +0530

    changes

--- frontend/src/nakama.js
+++ frontend/src/nakama.js
@@ -44,7 +44,7 @@ class Nakama {
 
             const trace = false;
             console.log("Creating socket...");
-            this.socket = this.client.createSocket(true, trace);
+            this.socket = this.client.createSocket(useSSL, trace);
             console.log("Socket created, connecting...");
             await this.socket.connect(this.session);
             console.log("Socket connected successfully");
@@ -92,7 +92,7 @@ class Nakama {
         sessionStorage.setItem("auth_type", "email");
 
         const trace = false;
-        this.socket = this.client.createSocket(true, trace);
+        this.socket = this.client.createSocket(useSSL, trace);
         await this.socket.connect(this.session);
         console.log("Socket connected for email auth");
     }
